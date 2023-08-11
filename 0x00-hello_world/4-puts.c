4619622cdf21f70a77ad1deab5fc5723988f7f57
Zillahh <ugwubonifaceotu1998@gmail.com>
2023-08-10 17:52:28 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: uses puts to print output
+ *
+ * return: 0 Everytime
+ *
+ */
+int main() {
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
