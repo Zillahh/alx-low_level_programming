6bd2e4a6d0f72398243e902a2fede0b3bc07ccfa
Zillahh <ugwubonifaceotu1998@gmail.com>
2023-08-10 17:58:43 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  * return: 0 Everytime
  *
  */
-int main() {
-	puts("Programming is like building a multilingual puzzle");
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
