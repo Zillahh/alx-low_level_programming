8cd3c94e4002a383516e7e1e668a75ac3458b4ec
Zillahh <ugwubonifaceotu1998@gmail.com>
2023-08-11 02:53:14 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints a standard e
+ *
+ * Return: 1(Success) Always
+ */
+int main(void)
+{
+	fput(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
