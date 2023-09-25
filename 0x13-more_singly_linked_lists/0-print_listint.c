62971b2000aed3a8a7ac8b807718db6bf0a2c553
Zillahh <ugwubonifaceotu1998@gmail.com>
2023-09-25 16:52:50 -0700

    okay

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,9 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	size_t numnodes = 0;
+
 	if (h == NULL)
 		return (0);
 
 	printf("%d\n", h->n);
+	numnodes++;
+
 	return (1 + print_listint(h->next));
 }
